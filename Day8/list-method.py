8c9d34182dfd743401e58cc341448553743c9e84
Abdullah Al Jubair <jubair2810@gmail.com>
2025-03-29 16:53:35 +0600

    added:day-8

---
+++ Day8/list-method.py
@@ -0,0 +1,43 @@
+
+# List add Method
+a = [1,2,3]
+b = [4,5,8]
+
+c = a+b
+print(c)
+
+# list () covert Method
+
+a = "Hellow Man"
+
+print(list(a))
+
+# append a value
+
+a = [1,45,20]
+a.append(29)
+print(a)
+
+
+# Insert Method
+
+a = [1,3,4]
+a.insert(1,2)
+print(a)
+
+# Copy method
+
+a = [2,4,2]
+b = a.copy()
+print(b)
+
+# Count Method
+
+a = [3,4,7,3]
+print(a.count(3))
+
+# Extend Method
+
+a = [1,2]
+a.extend([4,5,6])
+print(a)
---
+++ Day8/list-method2.py
@@ -0,0 +1,29 @@
+#  Pop Method
+
+a = [1,3,4,34]
+print(a.pop())
+
+# Remove
+
+a = [1,45,345,23]
+a.remove(45)
+print(a)
+
+# Reverse
+
+a = [1,2,3,4]
+a.reverse()
+print(a)
+
+
+# sort
+
+a = [23,1,34,0, 20]
+a.sort(reverse=True)
+print(a)
+
+
+# max Method
+
+a = [23,9880,30]
+print(max(a))
